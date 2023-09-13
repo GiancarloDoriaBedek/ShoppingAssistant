2170e359a5d3121742ed1174584dc9b42506750a
Giancarlo <giancarlo.bedek@hotmail.com>
2023-09-13 20:09:00 +0200

    Update unit fields on EF objects

---
+++ ShoppingAssistant/Migrations/20230913173158_Update unit fields.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingAssistant.Migrations
+{
+    public partial class Updateunitfields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyID",
+                table: "Packages");
+
+            migrationBuilder.RenameColumn(
+                name: "NativeID",
+                table: "Products",
+                newName: "ProductNativeID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "Packages",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PriceStartDate",
+                table: "Packages",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PriceEndDate",
+                table: "Packages",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PackageUnit",
+                table: "Packages",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Packages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Packages");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductNativeID",
+                table: "Products",
+                newName: "NativeID");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Unit",
+                table: "Packages",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PriceStartDate",
+                table: "Packages",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PriceEndDate",
+                table: "Packages",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "PackageUnit",
+                table: "Packages",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "CurrencyID",
+                table: "Packages",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
