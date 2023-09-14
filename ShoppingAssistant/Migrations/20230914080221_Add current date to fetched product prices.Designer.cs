b98a217d69bec43859f441c9149a2dc71c20374e
Giancarlo <giancarlo.bedek@hotmail.com>
2023-09-14 11:55:22 +0200

    Add current date to Packages

--- ShoppingAssistant/Migrations/20230914080221_Add current date to fetched product prices.Designer.cs
+++ ShoppingAssistant/Migrations/20230914080221_Add current date to fetched product prices.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoppingAssistant.Data;
 
@@ -11,9 +12,10 @@ using ShoppingAssistant.Data;
 namespace ShoppingAssistant.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230914080221_Add current date to fetched product prices")]
+    partial class Addcurrentdatetofetchedproductprices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
