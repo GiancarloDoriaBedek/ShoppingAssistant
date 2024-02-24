1eb4261e3a961f30586a13e16190ce10fa91db6b
Giancarlo <giancarlo.bedek@hotmail.com>
2024-02-24 14:54:48 +0100

    Extract mappings into separate file

---
+++ ShoppingAssistant/DependencyMapping.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ShoppingAssistant.Data;
+using ShoppingAssistant.Repository.Implementations;
+using ShoppingAssistant.Repository.Interfaces;
+using ShoppingAssistant.Services.Implementations;
+using ShoppingAssistant.Services.Interfaces;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
+namespace ShoppingAssistant
+{
+    public class DependencyMapping
+    {
+        private readonly WebApplicationBuilder _builder;
+
+        public DependencyMapping(string[] args) 
+        {
+            _builder = WebApplication.CreateBuilder(args);
+        }
+
+        public WebApplication BuildMappedWebApplication()
+        {
+            MapControllers();
+
+            MapAutoMapper();
+            MapHttpClient();
+
+            MapServices();
+            MapRepositories();
+
+            MapSwagger();
+            MapAuthentication();
+            MapDatabaseConnection();
+
+            return _builder.Build();
+        }
+
+        private void MapControllers()
+        {
+            _builder.Services.AddControllers();
+        }
+
+        private void MapAutoMapper()
+        {
+            _builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
+        }
+
+        private void MapHttpClient()
+        {
+            _builder.Services.AddHttpClient();
+        }
+
+        private void MapServices()
+        {
+            _builder.Services.AddScoped<IPasswordService, PasswordService>();
+            _builder.Services.AddScoped<ITokenService, TokenService>();
+            _builder.Services.AddScoped<IWebshopScraperService, WebshopScraperService>();
+            _builder.Services.AddScoped<IStringDistanceService, StringDistanceService>();
+            _builder.Services.AddScoped<IProductMappingService, ProductMappingService>();
+            _builder.Services.AddScoped<IProductService, ProductService>();
+        }
+
+        private void MapRepositories()
+        {
+            _builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            _builder.Services.AddScoped<IUserRepository, UserRepository>();
+            _builder.Services.AddScoped<IPackageRepository, PackageRepository>();
+            _builder.Services.AddScoped<IStoreRepository, StoreRepository>();
+            _builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+            _builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+        }
+
+        private void MapSwagger()
+        {
+            _builder.Services.AddEndpointsApiExplorer();
+            _builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+        }
+
+        private void MapAuthentication()
+        {
+            _builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                            .GetBytes(_builder.Configuration
+                                .GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
+
+
+        private void MapDatabaseConnection()
+        {
+            var connectionString = _builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+            _builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
--- ShoppingAssistant/Program.cs
+++ ShoppingAssistant/Program.cs
@@ -1,81 +1,11 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using ShoppingAssistant.Data;
-using ShoppingAssistant.Repository.Implementations;
-using ShoppingAssistant.Repository.Interfaces;
-using ShoppingAssistant.Services.Implementations;
-using ShoppingAssistant.Services.Interfaces;
-using Swashbuckle.AspNetCore.Filters;
-using System.Text;
-
 namespace ShoppingAssistant
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
-            builder.Services.AddHttpClient();
-
-            // Services
-            builder.Services.AddScoped<IPasswordService, PasswordService>();
-            builder.Services.AddScoped<ITokenService, TokenService>();
-            builder.Services.AddScoped<IWebshopScraperService, WebshopScraperService>();
-            builder.Services.AddScoped<IStringDistanceService, StringDistanceService>();
-            builder.Services.AddScoped<IProductMappingService, ProductMappingService>();
-            builder.Services.AddScoped<IProductService, ProductService>();
-
-            // Repositories
-            builder.Services.AddScoped<IProductRepository, ProductRepository>();
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-            builder.Services.AddScoped<IPackageRepository, PackageRepository>();
-            builder.Services.AddScoped<IStoreRepository, StoreRepository>();
-            builder.Services.AddScoped<IBrandRepository, BrandRepository>();
-            builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-                {
-                    Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
-                    In = ParameterLocation.Header,
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey
-                });
-                options.OperationFilter<SecurityRequirementsOperationFilter>();
-            });
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                            .GetBytes(builder.Configuration
-                                .GetSection("AppSettings:Token").Value)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
-
-            var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
-
-            builder.Services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlServer(connectionString);
-            });
-
-            var app = builder.Build();
+            var dependecyMapping = new DependencyMapping(args);
+            var app = dependecyMapping.BuildMappedWebApplication();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -89,7 +19,6 @@ namespace ShoppingAssistant
             app.UseAuthentication();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
